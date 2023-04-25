c37c19a8709bee1a2c75bdc7e6a915f96ef4556f
saderyan98 <saderyan98@hotmail.co.uk>
2023-04-25 16:20:38 +0100

    first commit

---
+++ variables.js
@@ -0,0 +1,37 @@
+// console.log("all around the world".charAt(7).toUpperCase())
+// let firstName = 'sade'
+// firstName = "catwoman"
+// console.log(firstName)
+
+// const lastName = 'ryan'
+// lastName = 'smith'
+// console.log(lastName)
+
+// let num1 = 2
+// let num2 = 3
+// num1 = num1 * 3
+// let result = num1 * num2
+// console.log(result)
+
+// num2 += 2
+// console.log(num2)
+
+let favoriteColour = "Red"
+let firstName = "Sade"
+
+console.log(`Hello, my first name is ${firstName}, my favorite colour is ${favoriteColour}`)
+
+console.log(`${firstName} prefers the colour ${favoriteColour}`)
+
+
+let breakfast = "pancakes"
+let lunch = "Tuna Sandwich"
+let dinner = "Sunday Roast"
+
+console.log(`Today, for breakfast I ate ${breakfast}, for lunch I ate a ${lunch} and dinner, ${dinner}`)
+
+breakfast = new "eggs"                                                                                                                                                                                                                                                                                                         
+lunch = new "crackers"
+dinner = new "Lasagne"
+
+console.log(`Tomorrow, for brekky I'll have ${breakfast}, for lunch I'm thinking ${lunch} and dinner I'm making ${dinner}`)
